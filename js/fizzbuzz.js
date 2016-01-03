9c0408b858aa347fa5bf5386141e386af10d68f9
Melanie Burger <melanie.brgr@gmail.com>
2016-01-03 18:23:57 -0500

    fizzbuzz program

---
+++ js/fizzbuzz.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	var ul = $('.fizzbuzzit');
+
+	for ( i = 1; i <= 100; i++ ) {
+		if ( i%3 === 0 && i%5 === 0 ) {
+			ul.append("<li>FizzBuzz</li>");
+		} else if ( i%3 === 0 ) {
+			ul.append("<li>Fizz</li>");
+		} else if ( i%5 === 0 ) {
+			ul.append("<li>Buzz</li>");
+		} else {
+			ul.append("<li>" + i + "</li>");
+		}
+	}
+
+});
